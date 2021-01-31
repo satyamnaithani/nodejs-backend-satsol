67a905cb369a7ec3b752645ef93d3bed57cc196d
satyamnaithani <you@example.com>
2021-01-31 23:14:28 +0530

    updated purchase model made vendor parameter compulsory

--- models/purchase.js
+++ models/purchase.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 
 const purchaseSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    item: { type: String },
+    item: { type: String, required: true },
     lotNo: { type: String },
     billNo: { type: String },
     exp: { type: Date },
-    vendor: { type: String },
-    quantity: { type: Number},
-    rate: { type: Number },
+    vendor: { type: String, required: true },
+    quantity: { type: Number, required: true},
+    rate: { type: Number, required: true },
     gst: { type: Number },
     purchaseRate: { type: Number },
     receiveDate: { type: Date },
     billDate: {type: Date},
-    uom: {type: String},
-    hsn: {type: String},
-    itemCode: {type: String},
+    uom: {type: String, required: true},
+    hsn: {type: String, required: true},
+    itemCode: {type: String, required: true},
     addedBy: {type: String}
 });
 
