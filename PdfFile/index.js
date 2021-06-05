70bdcb54d9548641fa5d6a9f253acc0a003ce8e5
Satyam <Naithani>
2021-06-05 17:39:04 +0530

    invoice structure correction

--- PdfFile/index.js
+++ PdfFile/index.js
@@ -258,9 +258,9 @@ module.exports = (pdfObj) => {
 								<div>PUNB0189020</div>
 							</div>
 						</div>
-						<div style="border: 1px solid grey; height: 112px; border-bottom: none; border-right: none; padding: 6px;">
+						<div style="border: 1px solid grey; height: 103px; border-bottom: none; border-right: none; padding: 6px;">
 							<div style="text-align: center;">for SATVIK SOLUTIONS</div><br/>
-							<div style="text-align: center; margin-top: 60px;">Authorised Signatory</div>
+							<div style="text-align: center; margin-top: 55px;">Authorised Signatory</div>
 						</div>
 					</div>
 				</div>
