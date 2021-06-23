1028b99ebbbcf9f83b1cdfc002221aafa2080208
Satyam <Naithani>
2021-06-23 23:47:10 +0530

    update

--- controllers/stock.js
+++ controllers/stock.js
@@ -2,7 +2,7 @@ const { execQuery } = require('../lib/commonFunctions');
 
 exports.get_all_stock = (req, res, next) => {
     const query = `
-    SELECT pt._id, i.name AS item, v.name AS vendor, pt.lot_no, DATE_FORMAT(pt.exp, "%d/%l/%Y") AS exp, pt.rate, i.gst, pt.quantity, pt.initial_quantity
+    SELECT pt._id, i.name AS item, v.name AS vendor, pt.lot_no, DATE_FORMAT(pt.exp, "%d/%l/%Y") AS exp, pt.rate, i.code, i.hsn, i.gst, pt.quantity, pt.initial_quantity
     FROM purchase_item pt 
     INNER JOIN purchase p
     ON pt.purchase_id = p._id
